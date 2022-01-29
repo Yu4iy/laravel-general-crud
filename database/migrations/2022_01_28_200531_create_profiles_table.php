54a9f2165c759cd9ce280b485db9cde85f684050
Yu4iy <yurimohyliuk@gmail.com>
2022-01-29 08:00:41 +0100

    ADD: profile-migration

--- database/migrations/2022_01_28_200531_create_profiles_table.php
+++ database/migrations/2022_01_28_200531_create_profiles_table.php
@@ -15,7 +15,21 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+				$table->string('first_name', 50);
+				$table->string('last_name', 50);
+				$table->tinyInteger('age');
+				$table->date('date_of_birth'); 
+				$table->string('gender', 10);
+				$table->string('country', 30);
+				$table->string('city', 30);
+				$table->string('address', 100);
+				$table->string('cell', 25);
+				$table->boolean('criminal');
+				$table->boolean('active_warants');
+				$table->string('slug');
+				
+				$table->timestamps();
         });
     }
 
